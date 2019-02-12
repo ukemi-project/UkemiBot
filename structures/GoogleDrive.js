ce0080091c1a710d1e53e52db26e64273baa8ff4
Dan <headhertz88@gmail.com>
2019-02-12 20:33:04 +0000

    Cleaned up nasty callback/promise shit - authentication is fucked

--- structures/GoogleDrive.js
+++ structures/GoogleDrive.js
@@ -7,47 +7,44 @@ class GoogleDrive {
     constructor() {
         this.rootFolder = '1JTapSPk1XNhxCKlJcOa53IW6QL6UjHa6';
         this.subFolder = {};
+        this.response = {};
     }
 
-    listFiles( message ) {
-        const scopes = [ 'https://www.googleapis.com/auth/drive.metadata.readonly' ],
+    async listFiles( message ) {
+        const scopes = [ 'https://www.googleapis.com/auth/drive' ],
             drive = google.drive( {
                 version: 'v3',
                 auth: googleClient.oAuth2Client
             } );
 
-        googleClient
-            .authenticate( scopes, message )
-            .then( () => {
-                let output = '= File List =\n\n';
+        let output = '= File List =\n\n',
+            files;
 
-                drive.files.list(
-                    {
-                        pageSize: 50,
-                        fields: 'nextPageToken, files(name)'
-                    },
-                    ( err, res ) => {
-                        if ( err ) {
-                            return console.log( `The API returned an error: ${err}` );
-                        }
+        await googleClient.authenticate( scopes, message );
 
-                        const files = res.data.files;
+        try {
+            this.response = await drive.files.list( {
+                pageSize: 25,
+                fields: 'nextPageToken, files(name)'
+            } );
+        } catch ( err ) {
+            return console.log( `The API returned an error: ${err}` );
+        }
 
-                        if ( files.length ) {
-                            files.forEach( ( file ) => {
-                                output += `• ${file.name}\n`;
-                            } );
-                        } else {
-                            message.reply( 'No files found.' );
-                        }
-                        message.channel.send( output, { code: 'asciidoc', split: { char: '\u200b' } } );
-                    }
-                );
-            } )
-            .catch( console.error );
+        files = this.response.data.files;
+
+        if ( !files.length ) {
+            message.reply( 'No files found.' );
+        }
+
+        files.forEach( ( file ) => {
+            output += `• ${file.name}\n`;
+        } );
+
+        message.channel.send( output, { code: 'asciidoc', split: { char: '\u200b' } } );
     }
 
-    uploadResource( message ) {
+    async uploadResource( message ) {
         /* TODO:
         *   Handle embed, and all attachment types.
         *   Work out a way to directly copy file instead of downloading and then uploading
@@ -59,59 +56,55 @@ class GoogleDrive {
                 auth: googleClient.oAuth2Client
             } );
 
-        googleClient
-            .authenticate( scopes, message )
-            .then( () => {
-                message.attachments.each( async( file ) => {
-                    await this.handleFolder( message );
+        await googleClient.authenticate( scopes, message );
+        await this.handleFolder( message );
 
-                    const fileStream = fs.createWriteStream( `${file.name}` ),
-                        fileMetadata = {
-                            name: `${file.name}`,
-                            parents: [ this.subFolder ]
-                        };
-                    let media = {};
+        message.attachments.each( async( file ) => {
+            await this.fileUploader( file, message );
+        } );
+    }
 
-                    await request
-                        .get( file.attachment )
-                        .on( 'response', ( response ) => {
-                            response.pipe( fileStream );
+    async fileUploader( file, message ) {
+        const fileStream = fs.createWriteStream( `${file.name}` ),
+            fileMetadata = {
+                name: `${file.name}`,
+                parents: [ this.subFolder ]
+            };
+        let media = {};
 
-                            media.mimeType = response.headers[ 'content-type' ];
-                        } )
-                        .on( 'error', ( err ) => {
-                            console.log( err );
-                        } );
-
-                    await drive.files.create(
-                        {
-                            requestBody: {},
-                            resource: fileMetadata,
-                            media: {
-                                body: fs.createReadStream( `${file.name}` )
-                            },
-                            fields: 'id'
-                        },
-                        ( err ) => {
-                            if ( err ) {
-                                console.error( err );
-                            } else {
-                                fs.unlink( `${file.name}`, ( error ) => {
-                                    if ( error ) {
-                                        console.log( error );
-                                    }
-                                } );
-
-                                message.react( '544264131780411437' );
-                                message.guild.channels
-                                    .get( message.settings.botLogChannel )
-                                    .send( ` uploaded ${file.name} from ${message.channel}.` );
-                            }
-                        }
-                    );
-                } );
+        await request
+            .get( file.attachment )
+            .on( 'response', ( response ) => {
+                response.pipe( fileStream );
+                media.mimeType = response.headers[ 'content-type' ];
             } )
-            .catch( console.error );
+            .on( 'error', ( err ) => {
+                console.log( err );
+            } );
+
+        try {
+            await drive.files.create( {
+                requestBody: {},
+                resource: fileMetadata,
+                media: {
+                    body: fs.createReadStream( `${file.name}` )
+                },
+                fields: 'id'
+            } );
+        } catch ( err ) {
+            console.error( `Error uploading ${file.name}, ${err}` );
+        }
+
+        fs.unlink( `${file.name}`, ( err ) => {
+            if ( err ) {
+                console.log( `Error deleting ${file.name}, ${err}` );
+            }
+        } );
+
+        message.react( '544264131780411437' );
+        message.guild.channels
+            .get( message.settings.botLogChannel )
+            .send( ` uploaded ${file.name} from ${message.channel}.` );
     }
 
     handleFolder( message ) {
