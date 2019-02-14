b82c6692f26b1ac82955bc5a9f605b12e9043a46
Dan <headhertz88@gmail.com>
2019-02-14 16:20:49 +0000

    Music activity updater

--- events/botReady.js
+++ events/botReady.js
@@ -1,10 +1,15 @@
 import Event from '../structures/Event.js';
+import ActivityUpdate from './../structures/ActivityUpdate';
 
 module.exports = class extends Event {
     async run() {
         if ( this.client.users.has( '1' ) ) {
             this.client.users.delete( '1' );
         }
+
+        setInterval( async() => {
+            await ActivityUpdate.fetch( this.client );
+        }, 20000 );
 
         console.log( `${this.client.user.tag}, ready to roll!` );
     }
---
+++ structures/ActivityUpdate.js
@@ -0,0 +1,46 @@
+import request from 'request-promise';
+
+class ActivityUpdate {
+    constructor() {
+        this.presence = {
+            status: 'online',
+            afk: 0,
+            activity: {
+                name: 'feedback.',
+                type: 'LISTENING'
+            }
+        };
+        // prettier-ignore
+        this.url = `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=LodestoneMusic&api_key=${process.env.LASTFM}&format=json&limit=1`;
+    }
+
+    async fetch( client ) {
+        let lastResponse, currentTrack;
+
+        try {
+            lastResponse = JSON.parse( await request.get( this.url ) ).recenttracks.track[ 0 ];
+        } catch ( e ) {
+            return console.error( e );
+        }
+
+        currentTrack = `\n${lastResponse.artist[ '#text' ]} - ${lastResponse.name}`;
+
+        if ( !lastResponse[ '@attr' ].nowplaying ) {
+            return;
+        }
+
+        if ( this.presence.activity.name === currentTrack ) {
+            return;
+        }
+
+        this.presence.activity.name = currentTrack;
+        this.update( client );
+    }
+
+    update( client ) {
+        client.user.setPresence( this.presence );
+        console.log( `Activity changed to: ${this.presence.activity.name}` );
+    }
+}
+
+export default new ActivityUpdate();
